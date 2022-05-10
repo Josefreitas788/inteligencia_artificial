c6ec6993c174ccf86cdfc86b6820f45f41946377
José Antônio <57533270+Josefreitas788@users.noreply.github.com>
2022-05-10 16:46:05 -0300

    Create Busca_Heuristica_ArvoreBinaria.py

---
+++ Busca_Heuristica_ArvoreBinaria.py
@@ -0,0 +1,58 @@
+from tree import drawTree 
+import random as rd
+
+class Node:
+
+z
+    self.left = left
+    self.right = right
+  
+  def insert(self, nome):
+        x = rd.randint(0,1)
+        if x == 0:
+            if self.left is None:
+               self.left = Node(nome)
+            else:
+               self.left.insert(nome)
+        else:
+               if self.right is None:
+                  self.right = Node(nome)
+               else:
+                  self.right.insert(nome)
+
+f =  open('cidades.txt')
+lista = f.read()
+lista = lista.split(',')
+lista.pop()
+#lista = [list(row) for row in lista.columns]
+print(lista)
+tree = Node('Brasil')
+for cidade in lista:
+      tree.insert(cidade)
+      
+
+drawTree(tree)
+
+nome = input("Digite um nome:")
+
+#Busca em largura
+queue = [tree]
+nomes = []
+
+while len(queue)!=0:
+  NoAtual = queue.pop(0)
+  nomes.append(NoAtual.nome)
+  if Atual.nome == nome:
+      print("Nome encontrado!")
+      queue.append("")
+      break   
+  if NoAtual.left!=None:
+    queue.append(NoAtual.left)
+  if NoAtual.right!=None:
+    queue.append(NoAtual.right)
+if len(queue) == 0:
+    print("O nome não foi encontrado")
+
+print("Os valores percorridos foram:{}".format(nomes))
+
+
